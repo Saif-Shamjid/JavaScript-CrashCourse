5a0aac6d1c1763ec87e5a4bc423ad93d8db11b68
Shamjid Jahan Saif <shamjid.saif@gmail.com>
2021-06-30 17:12:15 +0600

    Challenge-1 Completed!

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,15 @@
-console.log("This is a massage!");
+//challenge 1:
+
+function ageInDaysparam() {
+    var bornYear = prompt("What year were you born? ");
+    var allDays = (2021-bornYear)*365;
+    var mainResult = document.createElement('h2');
+    var mainTextResult = document.createTextNode('You are '+allDays+' Days old!');
+    mainResult.appendChild(mainTextResult);
+    mainResult.setAttribute('id','ageInDays-TextResult');
+    document.getElementById('ageInDays-result').appendChild(mainResult);
+}
+
+function resetAgeInDays() {
+    document.getElementById('ageInDays-TextResult').remove();
+}
