fd81c1a50e140fd15ecdb0370effb9710c29547b
Shamjid Jahan Saif <shamjid.saif@gmail.com>
2021-07-03 17:39:10 +0600

    Comlete RPS with mix!

--- static/js/script.js
+++ static/js/script.js
@@ -27,121 +27,115 @@ function generateCat() {
 // Challenge 3:
 
 function rpsGame(yourChoice) {
+    document.getElementById('rps-reset').setAttribute('onclick',`rpsResetClickHandler()`);
     // console.log(yourChoice.id);
     humanChoice = yourChoice.id;
     var botAsist = ['rock','paper','scissors'];
     botChoice = botAsist[Math.floor(Math.random()*botAsist.length)];
-    // result = decideWinner(humanChoice,botChoice); //return [0,1] human lose, bot win
-    // ansText = ansText(result); //{'message': 'you win', 'color': 'green'}
-    // rpsFontEnd(humanChoice,botChoice,ansText);
+    result = decideWinner(humanChoice,botChoice); //return [0,1] human lose, bot win
+    ansText = finalResult(result); //{'message': 'you win', 'color': 'green'}
+    rpsFontEnd(humanChoice,botChoice,ansText);
     
-    // mine!
-    result = decideWinner(humanChoice,botChoice); // 0(for human win) 1(for bot win) 1.5
-    mainAnsText = ansText(result); //'you win'
-    mainAnsColor = ansColor(result); 
-    rpsFontEnd(humanChoice,botChoice,mainAnsText,mainAnsColor);
-
-    // console.log(botChoice);
-    // console.log(result);
-    // console.log(mainAnsText);
-    // console.log(mainAnsColor);
+    
+    console.log(humanChoice);
+    console.log(botChoice);
+    console.log(result);
+    console.log(ansText);
+    
+    
 }
 
 function decideWinner(hc,bc) {
-    if (hc==bc) {
-        return 1.5;
-    }
-    else if (hc=="rock" && bc=="paper") {
-        return 1;
-    }
+    dataBase = {
+        'rock':{'rock':1.5,'paper':0,'scissors':1},
+        'paper':{'rock':1,'paper':1.5,'scissors':0},
+        'scissors':{'rock':0,'paper':1,'scissors':1.5}
 
-    else if(hc=="rock" && bc=="scissors") {
-        return 0;
-    }
-
-    else if (hc=="paper" && bc=="rock") {
-        return 0;
-    }
-
-    else if (hc=="paper" && bc=="scissors") {
-        return 1;
-    }
-    else if (hc=="scissors" && bc=="rock") {
-        return 1;
-    }
-    else if (hc=="scissors" && bc=="paper") {
-        return 0;
-    }
+    };
+    yourScore = dataBase[hc][bc];
+    botScore = dataBase[bc][hc];
+    return [yourScore,botScore];
 }
 
-function ansText(r) {
-    if (r==0) {
-        return "you win!";
-    }
-    else if (r==1) {
-        return "you lose!";
+function finalResult([hc,bc]) {
+    if (hc==0) {
+        return {'message': 'You lose', 'color': 'red'};
+    } else if (hc==1.5) {
+        return {'message': 'Match tie', 'color': 'yellow'};
     }
     else{
-        return "Match tie";
+        return {'message': 'You Win', 'color': 'green'};
     }
 }
 
-function ansColor(r) {
-    if (r==0) {
-        return "green";
+function  rpsFontEnd(hc,bc,mst) {
+    imageDataBase = {
+        'rock': document.getElementById('rock').src,
+        'paper': document.getElementById('paper').src,
+        'scissors': document.getElementById('scissors').src
+    };
+
+    if (mst['color']=='green') {
+        var shadow_cls_h = 'rpc-win';
+        var shadow_cls_b = 'rpc-lose';
     }
-    else if (r==1) {
-        return "red";
+    else if (mst['color']=='yellow') {
+        var shadow_cls_h = 'rpc-tie';
+        var shadow_cls_b = 'rpc-tie';
     }
     else{
-        return "yellow";
+        var shadow_cls_h = 'rpc-lose';
+        var shadow_cls_b = 'rpc-win';
     }
+
+    // Removing all eliment:
+
+    document.getElementById('hc').remove();
+    document.getElementById('bc').remove();
+    document.getElementById('ma').remove();
+
+    // Creating div
+    var hud = document.createElement('div');
+    hud.setAttribute("id", "jshd");
+    var mad = document.createElement('div');
+    mad.setAttribute("id", "jsmad");
+    var bud = document.createElement('div');
+    bud.setAttribute("id", "jsbd");
+    
+
+    hud.innerHTML = `<img class="${shadow_cls_h}" src="${imageDataBase[hc]}" height="150" width="173.734">`;
+
+    mad.innerHTML = `<h1 style="color:${mst['color']}; font-size:60px; padding:30px;">${mst['message']}</h1>`;
+
+    bud.innerHTML = `<img class="${shadow_cls_b}" src="${imageDataBase[bc]}"height="150" width="173.734">`;
+
+
+    document.getElementById('rpcc').appendChild(hud);
+    document.getElementById('rpcc').appendChild(mad);
+    document.getElementById('rpcc').appendChild(bud);
+    
 }
 
-function rpsFontEnd(hc,bc,mt,mc){
+function rpsResetClickHandler(mih,mib,mim){
+    document.getElementById('jshd').remove();
+    document.getElementById('jsmad').remove();
+    document.getElementById('jsbd').remove();
+    document.getElementById('rps-reset').removeAttribute('onclick');
+ 
+    var mih = document.createElement('div');
+    mih.setAttribute("id","hc");
+    var mib = document.createElement('div');
+    mib.setAttribute("id","ma");
+    var mim = document.createElement('div');
+    mim.setAttribute("id","bc");
 
-    var ans = document.createElement('h2');
-    var tAns = document.createTextNode(mt)
-    ans.appendChild(tAns);
-    ans.style.color=mc;
-    ans.setAttribute('id','rpc-ans')
-    var rock = document.getElementById('rock')
-    var paper = document.getElementById('paper')
-    var scissors = document.getElementById('scissors')
-    rock.remove();
-    paper.remove();
-    scissors.remove();
-    var contHc = document.getElementById('hc');
-    var contMa = document.getElementById('ma');
-    var contBc = document.getElementById('bc');
+    mih.innerHTML = `<img class="all-button" id="rock" src="https://www.seekpng.com/png/detail/816-8161371_rock-paper-scissor-icon-png.png" height="150" onclick="rpsGame(this)">`;
+    mib.innerHTML = `<img class="all-button" id="paper" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUCmaf-HaltNtvmtB6EG8jCjkbICmErLjlvACv-XM1yXMDVN5Tk_FJOKYdlGNCYaihdBk&usqp=CAU" height="150" onclick="rpsGame(this)">`;
+    mim.innerHTML = `<img class="all-button" id="scissors" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnGl8rflKvOAtHGAeTbVNihckaeR8d_FOdrXqpNAQJvsDbMfZm5DcpdB3P_7FGsOqU90o&usqp=CAU" height="150" onclick="rpsGame(this)">`;
 
+
+    document.getElementById('rpcc').appendChild(mih);
+    document.getElementById('rpcc').appendChild(mib);
+    document.getElementById('rpcc').appendChild(mim);
     
-    if (hc=='rock') {
-        contHc.appendChild((rock.cloneNode(true)))
-    }
-    else if (hc=='paper') {
-        contHc.appendChild((paper.cloneNode(true)));
-    }
-    else if(hc=='scissors'){
-        contHc.appendChild((scissors.cloneNode(true)));
-    }
-
-    contMa.appendChild(ans);
-
-    if (bc=='rock') {
-        contBc.appendChild((rock.cloneNode(true)));
-    }
-    else if (bc=='paper') {
-        contBc.appendChild((paper.cloneNode(true)));
-    }
-    else if(bc=='scissors'){
-        contBc.appendChild((scissors.cloneNode(true)));
-    }
-    
-    document.getElementById('rock').removeAttribute('onclick');
-    document.getElementById('paper').removeAttribute('onclick');
-    document.getElementById('scissors').removeAttribute('onclick');
-
-
 }
-
